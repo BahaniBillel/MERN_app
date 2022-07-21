351d05bd63c1b4b0f84d9423637c63cce3d292ad
BILLEL <billel.bahani@gmail.com>
2022-07-21 16:26:32 +0100

    adding routes

--- backend/server.js
+++ backend/server.js
@@ -13,10 +13,33 @@ app.use((req, res, next) => {
 
 // routes
 app.get('/', (req, res) => {
-  res.json({ mssg: 'Welcome to the app' });
+  res.json({ mssg: 'Welcome to the app home page' });
 });
 
-// listen for requests
+// GET a new request from WORKOUT
+
+app.get('/api/workouts', (req, res) => {
+  res.json({ mssg: ' GET a new request from workwout' });
+});
+
+// POSTE a new request WORKOUT
+app.post('/api/workouts', (req, res) => {
+  res.json({ mssg: ' POST  a new request' });
+});
+
+// UPDATE a new request WORKOUT
+
+app.patch('/api/workouts/:id', (req, res) => {
+  res.json({ mssg: ' UPDATE a new request from workout' });
+});
+
+// DELETE  a new request
+
+app.delete('/api/workouts/:id', (req, res) => {
+  res.json({ mssg: ' DELETE  a new request' });
+});
+
+// LISTEN for requests
 app.listen(4000, () => {
   console.log('listening on port', process.env.PORT);
 });
